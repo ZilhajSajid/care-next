0f1eac1478551324cd82345883a46bb55b85c5a1
Md.Zilhaj Un Noor <zilhajsajid@gmail.com>
2026-02-11 20:13:11 +0600

    google provider data is saved in DB

--- src/lib/authOptions.js
+++ src/lib/authOptions.js
@@ -1,6 +1,7 @@
 import { loginUser } from "@/actions/server/auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import { collections, dbConnect } from "./dbConnect";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -22,4 +23,34 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log({ user, account, profile, email, credentials });
+      const isExist = await dbConnect(collections.USERS).findOne({
+        email: user.email,
+        provider: account.provider,
+      });
+      if (isExist) {
+        return true;
+      }
+      const newUser = {
+        provider: account.provider,
+        name: user.name,
+        email: user.email,
+        image: user.image,
+        role: "user",
+      };
+      const res = await dbConnect(collections.USERS).insertOne(newUser);
+      return res.acknowledged;
+    },
+    // async redirect({ url, baseUrl }) {
+    //   return baseUrl;
+    // },
+    // async session({ session, token, user }) {
+    //   return session;
+    // },
+    // async jwt({ token, user, account, profile, isNewUser }) {
+    //   return token;
+    // },
+  },
 };
